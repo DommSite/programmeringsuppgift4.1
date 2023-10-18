2214b95ed604102320c2577e64b6284495393396
DommSite <johan.johansson06@live.se>
2023-10-18 10:11:56 +0200

    Add project files.

---
+++ programmeringsuppgift4.1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace inlämningsuppgift4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int förstatal = 10; förstatal <= 30; förstatal = förstatal + 1)
+            {
+                Console.WriteLine(förstatal);
+            }
+            Console.ReadKey();
+            for (int andratal = 200; andratal >= 180; andratal = andratal - 1)
+            {
+                Console.WriteLine(andratal);
+            }
+            Console.ReadKey();
+            for (int tredjetal = 1000; tredjetal <= 1400; tredjetal = tredjetal + 50)
+            {
+                Console.WriteLine(tredjetal);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
